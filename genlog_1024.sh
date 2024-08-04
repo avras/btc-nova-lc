85bdb128e135786716989ad6b1a9df4d0530b00e
Saravanan Vijayakumaran <sarva.v@gmail.com>
2024-08-04 18:09:35 +0530

    Added log generation scripts

---
+++ genlog_1024.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SLEEP=60s
+
+for i in {0..7}
+do
+    HDRS_PER_STEP=$(echo "2 ^ $i" | bc)
+    NUM_STEPS=$(echo "2^ (10 - $i)" | bc)
+    if [ $HDRS_PER_STEP -ne 1 ]; then
+        S="s"
+    else
+        S="" 
+    fi
+    echo "Generating logs for ${NUM_STEPS} steps with ${HDRS_PER_STEP} header${S} per step."
+    command time -v --output=./logs/time_output_${HDRS_PER_STEP}_${NUM_STEPS}.txt ./target/release/examples/validate_btc_header_batch scripts/headers_100000.txt $HDRS_PER_STEP $NUM_STEPS > ./logs/output_${HDRS_PER_STEP}_${NUM_STEPS}.txt
+    if [ $i -ne 7 ]; then
+        echo "Sleeping for 60 seconds to give the CPU a break"
+        sleep $SLEEP
+    fi
+done
+echo "See logs directory for output files"
